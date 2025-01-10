5ec32933847f1f8c1eca0b34bba18e0cdc28f29d
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-10 22:56:57 +0530

    updated commit

---
+++ List.py
@@ -0,0 +1,14 @@
+# bikes = ['hond_navy', 'hero_splender_plus', 'stunner', 'hond_livo']
+# bikes.append("platina")
+# bikes.pop()
+# bikes.remove("hond_navy")
+# bikes.copy()
+# print(bikes)
+# for bike in bikes:
+#     print(bike , end= ", ")
+
+# squared_num = [x**2 for x in range(10)]
+# print(squared_num)
+
+cube_num = [y**3 for y in range(5)]
+print(cube_num)
