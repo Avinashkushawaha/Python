7a5ea04facaa86eac2906c22547aecf93b8a2dc7
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-10 22:05:42 +0530

    first commit

---
+++ String_type.py
@@ -0,0 +1,29 @@
+# Institute = " Pyspider "
+# print(Institute.strip())
+
+# Name = "Nitin"
+# print(Name.replace("Nitin", "tiwari"))
+
+# vegies = "Tomato, Onion, Ginger, potato"
+# print(vegies.split(","))
+
+
+# Str = "my name is ram "
+# print(Str.find("my"))
+
+# vehicle = "cycle bike  train van train "
+# print(vehicle.count("train"))
+
+# chai_type = "Masala"
+# quantity = 2
+# order = "I ordered {} cups of {} chai"
+# print(order.format(quantity, chai_type))
+
+# Mobile_phone = ["oppo", "vivo", "Apple", "samsung", "nokia"]
+# print(", ".join(Mobile_phone))
+
+# chai = "he said, \"masala chai is awesome\" "
+# chai = '''he said, "masala chai is awesome" '''
+# chai = r"Avin\nchai"
+chai = r"c:\\user\\pwd\\"
+print(chai)
---
+++ data_type.py
@@ -0,0 +1,16 @@
+# num = 123
+# print(num)
+
+# set = {}
+# # print(set)
+# print(type(set))
+
+# a = set()
+# print(a)
+
+# tuple = (1, 2, 3, )
+# print(tuple)
+
+# dict = {'name':'avi'}
+# print(dict)
+# print(type(dict))
