5bac2297c8e26e8229645da9c1e84c1b4630ab06
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-10 23:45:48 +0530

    updated commit

---
+++ Dictonary_in_python.py
@@ -0,0 +1,19 @@
+fruits = {'apple':'1', 'mango':'2', 'stawberry':'3', 'Banana':'4' }
+# print(fruits.get("Banana"))
+# fruits['Banana'] = fruits.pop('papaya')
+# print(fruits)
+
+# for fruit in fruits:
+# for key, value in fruits.items():
+#     print(key, value)
+
+# if "apple" in fruits:
+#     print("I have apple fruits")
+
+
+
+
+
+for fruit in fruits:
+   
+ print(fruit, end=" ")    
