f31f6e5cbbc9e6617c28cff785edd12982660f3f
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-11 14:15:38 +0530

    update commit

--- Conditional_problem3.py
+++ Conditional_problem3.py
@@ -1,1 +1,25 @@
 # Assign a letter grade based on a student's score: A(90-100), B(80-89), C(70-79), D(60-69), f (below 60).
+score = float(input("Enter your score: "))
+
+if score >= 101:
+    print("Please Enter valid grade")
+
+    exit()
+
+if score >= 90:
+    grade =  "A"
+
+elif score >= 80:
+    grade = "B"
+
+elif score >= 70:
+    grade = "C"   
+
+elif score >= 60:
+    grade = "D"
+
+else:
+    grade = "f"
+
+print("Grade:", grade)        
+             
