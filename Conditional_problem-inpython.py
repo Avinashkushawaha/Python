c9ad5b1796c1e96d82fa7f5a4a5d800751091e04
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-11 13:19:23 +0530

    first commit

---
+++ Conditional_problem-inpython.py
@@ -0,0 +1,14 @@
+age  = float(input("Give me a score value: "))
+
+if age < 13:
+    print("child")
+elif age < 20:
+    print("Teenager")
+elif age < 60:
+    print("Adult")    
+else:
+    print("Senior")    
+
+
+
+
