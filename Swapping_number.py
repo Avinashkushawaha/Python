0092819fd5dd952e53d228b6907d63836b18b08d
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-11 12:31:20 +0530

    first commit

---
+++ Swapping_number.py
@@ -0,0 +1,29 @@
+# Swapping two numbera
+# a = 5
+# b = 10
+
+# a, b = b, a
+
+# print("after swapping:")
+# print("a=", a)
+# print("b=", b )
+
+# *****************
+
+# a = 5
+# b = 10
+
+# a = a ^ b
+# b = a ^ b
+# a = a ^ b
+
+# print("after swapping: a=", a, "b=", b)
+
+# a = 5
+# b = 10 
+# c = a
+# a = b
+# b = c
+# print("after swapping:")
+# print("a =", a)
+# print("b=", b)
