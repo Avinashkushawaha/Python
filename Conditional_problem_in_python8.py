3b9bf95fe1bbe788305dd91569d10f00542a5890
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-11 16:24:43 +0530

    first commit

--- Conditional_problem_in_python8.py
+++ Conditional_problem_in_python8.py
@@ -1,1 +1,16 @@
 # check if a password is "Weak", "Medium", or "Strong", criteria: <6 chars(Weak),6-10 chars (Medium), > 10 chars (Strong).
+password = "Avin123@#1"
+
+password_length = len (password)
+
+if len(password) < 6:
+    strength = "Weak"
+
+elif len(password) <= 10:
+     strength = "Medium"
+
+else :
+     strength = "Strong" 
+
+print("Password strength is: ", strength)
+                   
