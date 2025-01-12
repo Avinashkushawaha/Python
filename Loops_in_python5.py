22147a4efee19ce260b6dfdbb9096c56bfd431ba
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-12 22:38:54 +0530

    update commit

--- Loops_in_python5.py
+++ Loops_in_python5.py
@@ -1,1 +1,11 @@
 # Compute the factorial of a number using a while loop.
+number = 5
+fact = 1
+
+while number > 0:
+    # fact = fact * number
+    fact *= number 
+    number -= 1
+
+print("Factorials number value is :", fact)
+
