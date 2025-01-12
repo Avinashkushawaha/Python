9bdbc88581787a4d4572b3d64f417629a1dabc55
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-12 22:06:41 +0530

    update commit

--- Loops_in_python2.py
+++ Loops_in_python2.py
@@ -1,5 +1,9 @@
 # Print the multiplication table for a given number up to 10, but skip the fifth iteration.sum
-
+number = 3
+for i in range(1, 11):
+    if i == 5:
+        continue
+    print(number, 'x', i, '=', number * i) 
 
         
  
