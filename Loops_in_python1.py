efde795f5f62d7e07e4e0e603cb24d29692458ea
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-12 21:44:18 +0530

    update commit

--- Loops_in_python1.py
+++ Loops_in_python1.py
@@ -1,1 +1,9 @@
 # Calculate the sum of even numbers up to a given numbers n.
+n = 10 
+sum_even = 0
+
+for i in range(1, n+1):
+    if i%2 == 0:
+        sum_even += 1
+
+print("Summ of even number is: ",sum_even)        
