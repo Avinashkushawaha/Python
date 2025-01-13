63cbf3d91b65c4874cf98edb20b6c1b8e0ea73e0
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-13 23:49:34 +0530

    first commit

---
+++ Patter_python3.py
@@ -0,0 +1,9 @@
+# full star pyramid
+# for i in range(1, 6):
+#     print(''*(5 -i)+ '*' *(2 * i - 1))
+
+
+# Inverted full star pyramid
+# for i in range(5, 0, -1):
+#     print('' *(5 - i)+ '*' *(2 * i - 1))
+
