50d00989b3964923cc7e5e99702baa57f390d337
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-13 23:45:13 +0530

    update commit

--- Pattern_python2.py
+++ Pattern_python2.py
@@ -1,9 +1,20 @@
 # butterfly pattern
+# for i in range(1, 6):
+#     for j in range(1, i + 1):
+#         print('*', end="")
+#     for j in range(1, 2 * (5 - i)):
+#         print('', end="")
+#     for j in range(1, i + 1):
+#         print('*', end="") 
+#     print()       
+
+#Hollow number pyramid
 for i in range(1, 6):
-    for j in range(1, i + 1):
-        print('*', end="")
-    for j in range(1, 2 * (5 - i)):
+    for j in range(1, 6 - i):
         print('', end="")
-    for j in range(1, i + 1):
-        print('*', end="") 
-    print()           
+    for j in range(1, 2 * i):
+        if j == 1 or j == 2 * i - 1:
+            print(i, end='')    
+        else:
+            print('', end="")
+    print()        
