2db1db2d93d24ce64bd6d49ec49f469be0fdb661
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-13 23:19:37 +0530

    first commit

---
+++ Pattern_python1.py
@@ -0,0 +1,15 @@
+# Right angle triangle number pattern
+# for i in range(1, 6):
+#     print(''.join(str(x)for x in range(1, i + 1)))
+
+#Inverted right angle traingle number pattern 
+# for i in range(5,0, -1):
+#     print(''.join(str(x)for x in range(1, i + 1)))
+
+# floyd's triangle
+# num  = 1 
+# for i in range(1, 6):
+#     for j in range(1, i + 1):
+#         print(num, end="")
+#         num +=1
+#     print()    
