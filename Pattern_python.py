b202af33212f1100ed542064cc0ffc9641816652
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-13 23:10:19 +0530

    update commit

--- Pattern_python.py
+++ Pattern_python.py
@@ -3,7 +3,20 @@
 #     print(''*(5 -i)+ '*' *(2 * i -1))
 
 # inverted pyramid pattern
-for i in range(1, 6):
-    print(''*(5 - i)+ '*' *(2*i -1 ))
-for i in range(4, 0, -1):
-    print(''*(5 - i)+ '*' *(2*i-1))    
+# for i in range(1, 6):
+#     print(''*(5 - i)+ '*' *(2*i -1 ))
+# for i in range(4, 0, -1):
+#     print(''*(5 - i)+ '*' *(2*i-1))    
+
+# hollow square pattern
+# for i in range(5):
+#     for j in range(5):
+#         if i == 0 or i == 4 or j == 0 or j == 4:
+#             print('*', end="")
+#         else:
+#             print('',end="")
+#     print()            
+
+# full square pattern
+for i in range(5):
+    print('*' * 5)
