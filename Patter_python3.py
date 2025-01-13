f1e21e11d9903e72f80ab1b9795b76f34661bc48
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-13 23:52:18 +0530

    update commit

--- Patter_python3.py
+++ Patter_python3.py
@@ -7,3 +7,10 @@
 # for i in range(5, 0, -1):
 #     print('' *(5 - i)+ '*' *(2 * i - 1))
 
+# left aligned pyramid pattern
+# for i in range(1, 6):
+#     print('*' * i)
+
+# right aligned pyramid pattern
+for i in range(1, 6):
+    print(''*(5 - 1)+ '*' * i)
