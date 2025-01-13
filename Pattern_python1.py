be6d8b9c166898d7b9aa1551740df3a42201e3df
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-13 23:35:56 +0530

    update commit

--- Pattern_python1.py
+++ Pattern_python1.py
@@ -13,3 +13,35 @@
 #         print(num, end="")
 #         num +=1
 #     print()    
+
+# hollow right angle triangle
+# for i in  range(1, 5):
+#     for j in range(1, i + 1):
+#         if j == 1 or j == i or i == 5:
+#             print('*', end="")
+#         else:
+#             print('', end="")
+#     print()            
+
+# hollow pyramid pattern
+# for i in range(1, 6):
+#     for j in range(5 -i):
+#         print('', end="")
+#         for j in range(2 * i - 1):
+#             if j == 0 or j == 2 * i - 2 or i == 5:
+#                 print('*', end="")
+#             else:
+#                 print('', end="")
+#         print()            
+
+# hollow diamond pattern
+n = 5
+for i in range(1, n + 1):
+    for j in range(1, n - i + 1):
+        print('', end="")
+    for j in range(1, 2 * i):
+        if j == 1 or j == 2 * i - 1:
+            print(i, end="")
+        else:
+            print('', end="")
+    print()                
