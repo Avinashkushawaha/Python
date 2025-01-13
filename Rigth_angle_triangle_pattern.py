8a588308949289525f2af6fad0ef3eb406d12d25
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-13 22:57:05 +0530

    update commit

--- Rigth_angle_triangle_pattern.py
+++ Rigth_angle_triangle_pattern.py
@@ -2,3 +2,12 @@
 
 # for i in range(1,6):
 #     print('*' * i)
+
+
+# Inverted right angle triangle pattern
+# for i in range(5, 0, -1):
+#     print('*' * i)
+
+# pyramid pattern
+for i in range(1, 6):
+    print(''*(5 - i)+ '*' *(2 * i - 1))
