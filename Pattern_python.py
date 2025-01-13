d9df01f9254b137642305e73db11b04a9839d231
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-13 23:04:33 +0530

    first commit

---
+++ Pattern_python.py
@@ -0,0 +1,9 @@
+# Inverted pyramid pattern
+# for i in range(5, 0, -1):
+#     print(''*(5 -i)+ '*' *(2 * i -1))
+
+# inverted pyramid pattern
+for i in range(1, 6):
+    print(''*(5 - i)+ '*' *(2*i -1 ))
+for i in range(4, 0, -1):
+    print(''*(5 - i)+ '*' *(2*i-1))    
