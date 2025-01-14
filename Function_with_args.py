8c54abf0e2ede9df5fa8a48a7fac792081effe83
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-15 00:11:15 +0530

    first commit

---
+++ Function_with_args.py
@@ -0,0 +1,7 @@
+# write a function that takes variable number of arguments and their sum.
+def sum_all():
+    pass
+
+print(sum_all(1, 2))
+print(sum_all(1, 2, 3, 4, 5))
+print(sum_all(1, 2, 3, 4, 5, 6, 7, 8))
