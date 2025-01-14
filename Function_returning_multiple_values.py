05726cf808e081929a87b64021430a5d10ed31a3
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-14 23:28:09 +0530

    update commit

--- Function_returning_multiple_values.py
+++ Function_returning_multiple_values.py
@@ -1,1 +1,12 @@
 #Create a function that returns both the area and circumference of a circle given its radius.
+import math
+def circle_stats(radius):
+    area =  3.14 * radius ** 2
+    circumference = 2 * 3.14 * radius
+    return area, circumference
+
+a, c = circle_stats(3)
+
+print("Area:", a, "Circumference: ", c)
+
+
