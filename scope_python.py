4cab28451b8f161832849988b66627da49405348
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-16 00:05:04 +0530

    update commit

--- scope_python.py
+++ scope_python.py
@@ -1,19 +1,43 @@
 # scope 
-username = "chaiaurcode"
+# username = "chaiaurcode"
 
-def func():
-    #username = "chai"
-    print(username)
+# def func():
+#     #username = "chai"
+#     print(username)
 
 
-print(username)
-func()    
+# print(username)
+# func()    
 
 
 x = 99
-def func2(y):
-    z = x + y
-    return z
+# def func2(y):
+#     z = x + y
+#     return z
 
-result = func2(1)
-print(result)
+# result = func2(1)
+# print(result)
+
+# def func3():
+#     global x 
+#     x = 12
+
+# func3()
+# print(x)    
+
+# def f1():
+#     x = 88
+#     def f2():
+#         print(x)
+#     f2()
+# f1()        
+
+def chaicoder(num):
+    def actual(x):
+        return x ** 2
+    return actual
+
+f = chaicoder(2)
+g = chaicoder(3)
+
+print(f)
