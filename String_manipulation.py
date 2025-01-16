b249f37656c1d527a6a0e65875537cf93e278d3a
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-16 23:57:11 +0530

    update commit

---
+++ String_manipulation.py
@@ -0,0 +1,19 @@
+# a = "!!!Avinash!!!!!! Avi"
+# print(len(a))
+# print(a)
+# print(a.upper())
+# print(a.lower())
+# print(a.strip("!"))
+# print(a.rstrip("!"))
+# print(a.lstrip("!"))pr
+# print(a.replace("Avinash", "avi"))
+# print(a.split(" "))
+blogheading = "Introduction"
+# print(blogheading.capitalize())
+# print(blogheading.center(50))
+# print(blogheading.endswith("js."))
+# print(blogheading.find(""))
+# print(blogheading.index("o"))
+print(blogheading.isalnum())
+# print(blogheading.isalpha())
+# print(blogheading.is)
