54bf3cd2c5389112144e03a5a47052cce00677e0
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-17 23:48:31 +0530

    first commit

---
+++ loops.py
@@ -0,0 +1,11 @@
+# name = 'Abhisek'
+# for i in name:
+#     print(i)
+#     if(i == 'b'):
+#         print('This is something special!')
+
+colors = ['Red', 'Green', 'Blue', 'Yellow']
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
