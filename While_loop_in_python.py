e03e2c8176032930f9532606cdfcc39efbc275f9
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-18 23:16:12 +0530

    update commit

--- While_loop_in_python.py
+++ While_loop_in_python.py
@@ -1,2 +1,11 @@
-for i in range(3):
-    
+# i = int(input("Enter the number: "))
+# while(i<=38):
+#     i = int(input("Enter the number: "))
+#     print(i)
+
+# print("Done with the loop")    
+
+count = 5
+while (count > 0):
+    print(count)
+    count = count - 1
