70d6398f71f582b7f0d54bfcf58f2d81702695fa
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-18 23:39:52 +0530

    update commit

--- break_and_continue.py
+++ break_and_continue.py
@@ -5,9 +5,16 @@
 
 # print("Loop ko chhodkar nikal gaya")    
 
-for i in range(12):
-    if(i == 10):
-        print("Skip the iteration")
-        continue
-    print("5 X", i+1, "=", 5 *(i+1))
+# for i in range(12):
+#     if(i == 10):
+#         print("Skip the iteration")
+#         continue
+#     print("5 X", i+1, "=", 5 *(i+1))
 
+
+i = 0
+while True:
+    print(i)
+    i = i + 1
+    if(i%101 == 0):
+        break
