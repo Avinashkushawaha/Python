da2b417fafbfc7b340a17bcbb5da40d4284e5e88
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-18 23:55:54 +0530

    update commit

--- Function_in_python.py
+++ Function_in_python.py
@@ -1,15 +1,18 @@
 def calculateGmean(a, b):
     mean = (a*b)/(a+b)
     print(mean)
+
+def isGreater(a, b):    
+    if(a>b):
+        print("first number is greater")
+    else:
+        print("Second number is greater")    
 a = 9
 b = 8
+isGreater(a, b)
 # gmean = (a*b)/(a+b)
 # print(gmean)
 # calculateGmean(a, b)
-if(a>b):
-    print("first number is greater")
-else:
-    print("Second number is greater")    
 
 # c = 8
 # d = 7
