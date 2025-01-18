a785dbbfb276799d76a35d718efe5f5494186788
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-18 23:51:24 +0530

    update commit

--- Function_in_python.py
+++ Function_in_python.py
@@ -5,9 +5,13 @@ a = 9
 b = 8
 # gmean = (a*b)/(a+b)
 # print(gmean)
-calculateGmean(a, b)
+# calculateGmean(a, b)
+if(a>b):
+    print("first number is greater")
+else:
+    print("Second number is greater")    
 
-c = 8
-d = 7
-calculateGmean(c, d)
+# c = 8
+# d = 7
+# calculateGmean(c, d)
 
