4fe55106e6e386e19df6997c1892d4800015e91a
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-18 23:48:34 +0530

    update commit

--- Function_in_python.py
+++ Function_in_python.py
@@ -1,5 +1,13 @@
+def calculateGmean(a, b):
+    mean = (a*b)/(a+b)
+    print(mean)
 a = 9
 b = 8
-gmean = (a*b)/(a+b)
+# gmean = (a*b)/(a+b)
+# print(gmean)
+calculateGmean(a, b)
 
-print(gmean)
+c = 8
+d = 7
+calculateGmean(c, d)
+
