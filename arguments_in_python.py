dcddb80a471594f2fe9cf2b405e279ada93ab376
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-19 23:11:59 +0530

    update commit

--- arguments_in_python.py
+++ arguments_in_python.py
@@ -1,1 +1,15 @@
-de 
+# def average(a, b, c=1):
+#     print("The average is ", (a+b+c) / 2)
+
+# average(5, 6)
+
+
+# def average(*numbers):
+#     sum = 0
+#     for i in numbers:
+#         sum = sum + i
+#     print("Average is: ", sum/ len(numbers))
+
+
+# average(5, 6, 7, 1)
+
