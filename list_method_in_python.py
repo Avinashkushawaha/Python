27e521e908d625959e1a112d6b5c06c4ebceb251
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-19 23:50:29 +0530

    update commit

--- list_method_in_python.py
+++ list_method_in_python.py
@@ -1,3 +1,8 @@
 list = [11, 45, 1, 2, 4, 6]
-print(list)
-list.append
+# print(list)
+list.append(7)
+# list.sort(reverse=True)
+# print(list.reverse())
+# print(list.index(1))
+print(list.count(1))
+print
