9a29bffe3deb292c57fd3344198e8730aaf89cb5
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-20 23:52:59 +0530

    update commit

--- f_string_in_pyton.py
+++ f_string_in_pyton.py
@@ -3,3 +3,7 @@ country= "India"
 name= "Harry"
 
 print(letter.format(name, country))
+print(f"hey my name is {name} and I am from {country}")
+price = 49.0999
+txt = f"For only {price:.2f} dollers!"
+print(txt)
