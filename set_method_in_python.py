1c3002a8dbc7ed62ed672655b8aa8709459a9fe0
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-22 22:51:25 +0530

    update commit

--- set_method_in_python.py
+++ set_method_in_python.py
@@ -1,5 +1,10 @@
-s1 = {1, 2, 5, 6}
-s2 = {2, 5, 7, 8}
-print(s1.union(s2))
-s1.update(s2)
-print(s1, s2)
+# s1 = {1, 2, 5, 6}
+# s2 = {2, 5, 7, 8}
+# print(s1.union(s2))
+# s1.update(s2)
+# print(s1, s2)
+
+cities = {'Bangalore', 'Mumbai', 'Delhi'}
+cities1 = {'pune', 'noida', 'delhi'}
+cities3 = cities.union(cities1)
+print(cities3)
