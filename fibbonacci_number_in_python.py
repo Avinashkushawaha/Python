644102a9171d86e35939d4e3c176bc56dd76545b
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-22 22:19:30 +0530

    update commit

--- fibbonacci_number_in_python.py
+++ fibbonacci_number_in_python.py
@@ -1,3 +1,8 @@
 # find fibbonacci number in python using recursion
- 
+def fibbonacci(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fibbonacci(n-1) + fibbonacci(n-2)    
+print(fibbonacci(10))
     
