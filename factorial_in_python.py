c9a4c10231598aee8ee9e6ee930cf557552f66ab
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-22 22:10:41 +0530

    update commit

--- factorial_in_python.py
+++ factorial_in_python.py
@@ -1,1 +1,7 @@
-# find foactorial of a number in python
+# find foactorial of a number in python using recursion
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+print(factorial(5))
