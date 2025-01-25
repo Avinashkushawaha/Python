2ef6fa98a169e9a2f427ef64a80d719bb1837b16
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-25 21:15:41 +0530

    update commit

--- Exception_handling.py
+++ Exception_handling.py
@@ -1,2 +1,9 @@
 a = input("Enter the number: ")
-print(f"Multiplication of {a} with 2 is {a*2}")
+print(f"Multiplication of {a}  is: ")
+try:
+  for i in range(1, 11):
+    print(f'{a} * {i} = {a*i}')
+except:
+    print("Please enter a valid number")
+print('some imp lines of code ')
+print("End of the program")
