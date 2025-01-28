04b481bf82b563786c0bfc82b907c062148bca2a
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-28 23:31:05 +0530

    update commit

--- Import_pandas.py
+++ Import_pandas.py
@@ -1,6 +1,14 @@
 # import math
 # math.floor(4.2343)
-from math import *
+# from math import *
 
-reuslt = sqrt(9) * pi
-print(reuslt)
+# reuslt = sqrt(9) * pi
+# print(reuslt)
+
+# import math
+
+# print(dir(math))
+# print(math.nan, type(math.nan))
+
+def welcome():
+    print("Welcome to Python")
