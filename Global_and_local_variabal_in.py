a6bfd26e25ffece5434c6deb42537cfdc5922d4d
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-29 22:45:45 +0530

    update commit

--- Global_and_local_variabal_in.py
+++ Global_and_local_variabal_in.py
@@ -2,4 +2,11 @@ x = 4
 print(x)
 
 def hello():
-    print(x)
+    x = 5
+    print(f"The lacoal value of x is {x}")
+    print(f"Hello developer")
+
+print(f"The global value of x is {x}")
+hello()
+x = 6
+print(f"The global value of x is {x}")    
