76ff79c78ce1c8f418d791267e9bfc0ccef6f0eb
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-31 00:20:18 +0530

    update commit

--- Map_filter_Reduce_in.py
+++ Map_filter_Reduce_in.py
@@ -1,12 +1,21 @@
-def cube(x):
-    return x * x * x
+# def cube(x):
+#     return x * x * x
 
-print(cube(2))
+# print(cube(2))
 
-l = [1, 2, 4, 6, 4, 3]
-newl = []
-for item in l:
-    newl.append(cube(item))
+# l = [1, 2, 4, 6, 4, 3]
+# newl = []
+# for item in l:
+#     newl.append(cube(item))
             
-newl = list(map(cube, l))
-print(newl)            
+# newl = list(map(cube, l))
+# print(newl)            
+
+from functools import reduce
+
+numbers = [1, 2, 3, 4, 5]
+
+def mysum(x, y):
+    return x + y
+sum = reduce(mysum, numbers)
+print(sum)
