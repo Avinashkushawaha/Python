e4ac95f94b470966a89878801edc9fa0e63c6fe8
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-31 23:42:28 +0530

    update commit

--- Oops_in_python.py
+++ Oops_in_python.py
@@ -6,8 +6,11 @@ class Person:
         print(f"{self.name} is a {self.occupation}")
 
 a = Person()
+b = Person()
 # a.name = 'shubham'
 a.occupation = 'Accountant'
-print(a.name, a.occupation)
-
+b.name = 'gitika'
+b.occupation = 'manager'
+# print(a.name, a.occupation)
+b.info()
 a.info()
