17c9f1c143b4a8064c78642b7092f78a13856dae
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-31 23:34:14 +0530

    update commit

---
+++ Oops_in_python.py
@@ -0,0 +1,9 @@
+class Person:
+    name = 'Harry'
+    occupation = 'Software Developer'
+    networth = 10
+
+a = Person()
+# a.name = 'shubham'
+# a.occupation = 'avi'
+print(a.name, a.occupation)   
