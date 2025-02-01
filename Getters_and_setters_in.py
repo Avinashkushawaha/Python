dddece5e024b8da8356c0831543fcc24b589bf80
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-01 17:56:24 +0530

    update commit

--- Getters_and_setters_in.py
+++ Getters_and_setters_in.py
@@ -1,14 +1,23 @@
 class MyClass:
     def __init__(self, value):
         self._value = value
+
     def show(self):
-        print(f"value is{self.value}")
+        print(f"value is{self._value}")
 
     @property
-    def value(self):
-        return self._value  
+    def ten_value(self):
+        return 10* self._value  
+    
+    @ten_value.setter
+    def ten_value(self, new_value):
+        self._value = new_value/10
+        
     
 obj = MyClass(10)
-print(obj._value)
+obj.ten_value = 67
+print(obj.ten_value)
 obj.show()    
-
+    
+    
+    
