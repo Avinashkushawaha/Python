ab97faf84220201eb00f6dac73e7bf7a4d038d89
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-03 23:16:01 +0530

    initial commit

--- Instance_variable_vs_class_variable_in_python.py
+++ Instance_variable_vs_class_variable_in_python.py
@@ -1,8 +1,12 @@
 class Employee:
+    companyName = "Apple"
+    noOfEmployees = 0
     def __init__(self, name):
         self.name = name
+        self.raise_amount = 0.02
+        Employee.noOfEmployees += 1
     def showDetails(self):
-        print(f"The name of the Employee is {self.name}") 
+        print(f"The name of the Employee is {self.name} and the raise amount in {self.noOfEmployees} sized {self.companyName} is {self.raise_amount}")
 
 emp1 = Employee("Harry")
 # Employee.showDetails(emp1)        
