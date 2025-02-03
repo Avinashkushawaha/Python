cbd2536e6808b1ec10a6587c5793d59508a66055
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-03 22:50:43 +0530

    update commit

--- Static_methods_in_python.py
+++ Static_methods_in_python.py
@@ -9,5 +9,10 @@ class Math:
     def add(a, b):
         return a + b
     
-result = Math.add(1, 2)
-print(result)    
+# result = Math.add(1, 2)
+# print(result)    
+
+a = Math(5)
+print(a.num)
+a.addtonum(6)
+print(a.num)
