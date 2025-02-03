a3fe94ace222aa98da370e178a46a39b60fbb6e5
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-03 23:06:12 +0530

    first update

--- Instance_variable_vs_class_variable_in_python.py
+++ Instance_variable_vs_class_variable_in_python.py
@@ -5,4 +5,8 @@ class Employee:
         print(f"The name of the Employee is {self.name}") 
 
 emp1 = Employee("Harry")
-Employee.showDetails(emp1)        
+# Employee.showDetails(emp1)        
+emp1.raise_amount = 0.3
+emp1.showDetails()
+emp2 = Employee("Rohan")
+emp2.showDetails()
