8cb809e1567d67a56d894c06117d3f40b2c6bb09
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-05-15 09:41:53 +0530

    update commit

--- Problem7.py
+++ Problem7.py
@@ -1,5 +1,15 @@
 #write a program to accept marks of 6 students and disply them in a sorted manner
 
-student_marks = [45, 31, 49, 32, 42, 54]
-student_marks.sort()
-print(student_marks)
+# student_marks = [45, 31, 49, 32, 42, 54]
+# student_marks.sort()
+# print(student_marks)
+
+#write a program to sum a list with numbers
+# l = [3, 4, 5, 6, 7]
+# print(sum(l))
+
+
+# write a program to count the number of zeros in the folllowing tuple.
+zero_list = (0, 2, 0, 2, 0, 9)
+a = zero_list.count(0)
+print(a)
