7b196e4c4718b8500afa992ec11d9d7209df0ed5
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-05-15 10:47:04 +0530

    update commit

---
+++ Problem8.py
@@ -0,0 +1,47 @@
+# write a program to create a dictionary of hindi words with values as their English translation Provide user with an option to look it up.
+
+# words = {
+#     "madad": "help",
+#     "kursi": "chair",
+#     "hathi": "elephant"
+#     }
+
+# word = input("Enter the word you want meaning of: ")
+# print(words[word])
+
+# Write a program to input eight numbers from the user and display all the unique numbers(once).
+# s = set()
+# n = input("Enter number:")
+# s.add(int(n))
+# n = input("Enter number:")
+# s.add(int(n))
+# n = input("Enter number:")
+# s.add(int(n))
+# n = input("Enter number:")
+# s.add(int(n))
+# n = input("Enter number:")
+# s.add(int(n))
+# print(s)
+
+# Create an empty dictionary. allow 4 friends to enter thier favorite language as value and use thier names assume that the names are unique.
+# d = {}
+
+# name = input("Enter name here: ")
+# lang = input("Enter language name : ")
+# d.update({name : lang})
+# name = input("Enter name here: ")
+# lang = input("Enter language name : ")
+# d.update({name : lang})
+# name = input("Enter name here: ")
+# lang = input("Enter language name : ")
+# d.update({name : lang})
+# name = input("Enter name here: ")
+# lang = input("Enter language name : ")
+# d.update({name : lang})
+# name = input("Enter name here: ")
+# lang = input("Enter language name : ")
+# d.update({name : lang})
+# name = input("Enter name here: ")
+# lang = input("Enter language name : ")
+# d.update({name : lang})
+# print(d)
