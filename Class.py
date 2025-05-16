d8d13c1dec5eec42b66e7bb2bb4462e592de7f63
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-05-16 15:26:25 +0530

    update commit

---
+++ Class.py
@@ -0,0 +1,11 @@
+class Employee:
+    language = "Py"
+    salary = 1200000
+
+harry = Employee()
+harry.name = "Harry"
+print(harry.name, harry.language, harry.salary)
+
+rohan = Employee()
+rohan.name = "Rohan Roro robinson"
+print(rohan.name, rohan.salary, rohan.language)
