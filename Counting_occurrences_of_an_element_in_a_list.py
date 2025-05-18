bb8bf5f1a9df2c475436fc980ae3d1c7f09988e7
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-05-18 22:25:32 +0530

    update count

---
+++ Counting_occurrences_of_an_element_in_a_list.py
@@ -0,0 +1,14 @@
+# def count_occurence(lst, element):
+#     return lst.count(element)
+
+# print(count_occurence([1, 2, 2,  3, 4, 5, 2], 2))
+
+# def count_occurence(lst, element):
+#     return lst.count(element)
+
+# print(count_occurence([2, 2, 2, 3, 4, 5, 3], 3))
+
+def count_occurence(lst, element):
+    return lst.count(element)
+
+print(count_occurence([1, 2, 3, 4, 4, 5, 4, 5], 5))
