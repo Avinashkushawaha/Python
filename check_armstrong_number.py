92ebf899e66699ad7868c8f27873ec4ab0ed191e
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-05-18 21:52:37 +0530

    update commit

---
+++ check_armstrong_number.py
@@ -0,0 +1,10 @@
+def is_armstrong_number(n):
+    num_str = str(n)
+    num_digits = len(num_str)
+    return sum(int(digit)** num_digits for digit in num_str) == n
+
+print(is_armstrong_number(153))
+print(is_armstrong_number(123))
+                     
+                     
+                     
