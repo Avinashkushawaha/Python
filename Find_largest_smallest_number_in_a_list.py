e8f0e1ed7b56883520f0b4feda890f1afabb9016
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-05-18 22:15:18 +0530

    update commit

---
+++ Find_largest_smallest_number_in_a_list.py
@@ -0,0 +1,8 @@
+def find_largest(nums):
+    return max(nums)
+
+def find_smallest(nums):
+    return min(nums)
+
+print(find_largest([10, 20, 30, 40, 50]))
+print(find_smallest([10, 20, 30, 40, 50]))
