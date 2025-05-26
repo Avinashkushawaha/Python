af18f05eda2cf1ff48b662569974105c4526385d
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-05-26 13:07:52 +0530

    update commit

---
+++ Check_for_prime_in_Range.py
@@ -0,0 +1,8 @@
+def primes_in_range(start, end):
+    primes = []
+    for num in range(start, end+1):
+        if (num): 
+            primes.append(num)
+    return primes
+
+print(primes_in_range(10, 30))
