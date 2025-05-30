0e829c1aa7b20dc959bb16020bc6c8e8e95ace29
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-05-30 19:57:10 +0530

    update commit

---
+++ Simp[le_calculator_using_oops.py
@@ -0,0 +1,9 @@
+class Calculator:
+    def add(self, a, b): return a + b
+    def subtract(self, a, b): return a - b
+    def multiply(self, a, b): return a * b
+    def divide(self, a, b): return a / b if b != 0 else "Error"
+
+calc = Calculator()
+print(calc.add(10, 5))       # 15
+print(calc.divide(10, 0))    # Error
