25c1a4bd573fc887a45761a26c24561248ef4677
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-05-31 12:00:30 +0530

    update commit

--- List_node.py
+++ List_node.py
@@ -3,9 +3,36 @@ class ListNode:
         self.val = val
         self.next = next
 
-    def printList(head):
-        while head:
-            print(head.val, end=" -> ")
-            head = head.next
-            
-        print("None")        
+def printList(head):
+    while head:
+        print(head.val, end=" -> ")
+        head = head.next
+    print("None")
+
+class Solution:
+    def reverseList(self, head):
+        prev = None
+        curr = head
+        
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        
+        return prev
+    
+node5 = ListNode(5)
+node4 = ListNode(4, node5)
+node3 = ListNode(3, node4)
+node2 = ListNode(2, node3)
+head = ListNode(1, node2)
+
+print("Original List:")
+printList(head)
+
+sol = Solution()
+reversed_head = sol.reverseList(head)
+
+print("Reversed List:")
+printList(reversed_head)
