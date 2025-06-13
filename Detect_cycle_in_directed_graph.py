f736fc8c3bc82484eb99937ff824a123e7a8283e
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-06-13 10:24:50 +0530

    update commit

---
+++ Detect_cycle_in_directed_graph.py
@@ -0,0 +1,26 @@
+def has_cycle(graph):
+    visited = set()
+    rec_stack = set()
+
+    def dfs(v):
+        visited.add(v)
+        rec_stack.add(v)
+
+        for neighbor in graph[v]:
+            if neighbor not in visited:
+                if dfs(neighbor):
+                    return True
+            elif neighbor in rec_stack:
+                return True    
+        
+        rec_stack.remove(v)
+        return False
+    
+    for node in graph:
+        if node not in visited:
+            if dfs(node):
+                return True
+    return False 
+
+graph = {'A': ['B'], 'B' : ['C'], 'C': ['A']}        
+print(has_cycle(graph))
