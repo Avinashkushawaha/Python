4ccf0232d21ffcf45585516e19c9db13c329f5bd
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-06-15 11:28:03 +0530

    update commit

---
+++ Custom_iterator_for_fibonacci.py
@@ -0,0 +1,17 @@
+class Fibonacci:
+    def __init__(self, max):
+        self.max = max
+        self.a, self.b = 0, 1
+
+    def __iter__(self):
+        return self 
+
+    def __next__(self):
+        if self.a > self.max:
+            raise StopIteration
+        val = self.a
+        self.a, self.b = self.b, self.a + self.b
+        return val
+
+for num in Fibonacci(20):
+    print(num, end=' ')      
