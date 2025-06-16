d2711aaf141de53c983e258a2300ca52092e06a0
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-06-16 18:19:55 +0530

    update commit

--- Longest_common_prefix.py
+++ Longest_common_prefix.py
@@ -1,1 +1,13 @@
-def long
+def longest_common_prefix(strs):
+    # Return an empty string if the input list is empty
+    if not strs:
+        return ""
+    
+    shortest = min(strs, key=len)
+    for i in range(len(shortest)):
+        # Check if the current character is the same in all strings
+        if any(s[i] != shortest[i] for s in strs):
+            return shortest[:i]
+    return shortest
+
+print(longest_common_prefix(["flower", "flow", "flight"]))
