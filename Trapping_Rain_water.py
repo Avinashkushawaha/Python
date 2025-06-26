e8afb9bf98513ccbefad69004d7e8d1018420a8e
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-06-26 21:54:00 +0530

    update commit

---
+++ Trapping_Rain_water.py
@@ -0,0 +1,15 @@
+def trap(height):
+    left, right = 0, len(height) - 1
+    lmax = rmax = water = 0
+    while left < right:
+        if height[left] < height[right]:
+            lmax = max(lmax, height[left])
+            water += lmax - height[left]
+            left += 1
+        else:
+            rmax = max(rmax, height[right])
+            water += rmax - height[right]
+            right -= 1
+    return water
+
+print(trap([0,1,0,2,1,0,1,3,2,1,2,1]))  # 6
