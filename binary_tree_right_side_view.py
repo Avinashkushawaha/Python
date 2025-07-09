c20810d821730bc637836eabb9cdaf7d3b72b93a
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-07-09 19:32:17 +0530

    update commit

--- binary_tree_right_side_view.py
+++ binary_tree_right_side_view.py
@@ -1,1 +1,19 @@
 
+from collections import deque
+
+def right_side_view(root):
+    if not root:
+        return []
+    res = []
+    q = deque([root])
+    while q:
+        level_size = len(q)
+        for i in range(level_size):
+            node = q.popleft()
+            if i == level_size - 1:
+                res.append(node.val)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+    return res
