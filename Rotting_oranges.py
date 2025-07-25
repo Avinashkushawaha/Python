97794df9f98a7e0289b438dae6db9d881b4ebeab
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-07-25 21:08:27 +0530

    update commit

---
+++ Rotting_oranges.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+def orangesRotting(grid):
+    rows, cols = len(grid), len(grid[0])
+    queue = deque()
+    fresh = 0
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 2:
+                queue.append((r, c, 0))
+            elif grid[r][c] == 1:
+                fresh += 1
+
+    time = 0
+    while queue:
+        r, c, t = queue.popleft()
+        for dr, dc in [(-1,0),(1,0),(0,-1),(0,1)]:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                grid[nr][nc] = 2
+                fresh -= 1
+                queue.append((nr, nc, t + 1))
+        time = max(time, t)
+    return time if fresh == 0 else -1
