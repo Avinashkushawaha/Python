1b4ab96c52447754071dcdfa62a926e7f77f2ce0
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-07-27 22:48:12 +0530

    update commit

---
+++ find_eventual_safe_state.py
@@ -0,0 +1,15 @@
+def eventualSafeNodes(graph):
+    n = len(graph)
+    color = [0] * n
+
+    def dfs(node):
+        if color[node] > 0:
+            return color[node] == 2
+        color[node] = 1
+        for nei in graph[node]:
+            if not dfs(nei):
+                return False
+        color[node] = 2
+        return True
+
+    return [i for i in range(n) if dfs(i)]
