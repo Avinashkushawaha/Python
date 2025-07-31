d64443869a008d4921daa2d9ca245baecf4f62b0
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-07-31 18:16:29 +0530

    update commit

---
+++ best_time_to_buy_and_sell_stock_with_transaction_fee.py
@@ -0,0 +1,6 @@
+def maxProfit(prices, fee):
+    cash, hold = 0, -prices[0]
+    for price in prices[1:]:
+        cash = max(cash, hold + price - fee)
+        hold = max(hold, cash - price)
+    return cash
