922eb09b3aab322a9a3200b0fc7375966a29f4a9
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-08-02 21:18:59 +0530

    update commit

---
+++ optimal_strategy_for_a_game.py
@@ -0,0 +1,13 @@
+def optimalStrategy(coins):
+    n = len(coins)
+    dp = [[0]*n for _ in range(n)]
+
+    for gap in range(n):
+        for i in range(n-gap):
+            j = i + gap
+            x = dp[i+2][j] if (i+2) <= j else 0
+            y = dp[i+1][j-1] if (i+1) <= (j-1) else 0
+            z = dp[i][j-2] if i <= (j-2) else 0
+            dp[i][j] = max(coins[i] + min(x, y), coins[j] + min(y, z))
+
+    return dp[0][n-1]
