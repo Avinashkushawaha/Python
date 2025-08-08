5664af1634f8d8ce3d56d6c5289b98c610524386
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-08-08 23:25:38 +0530

    update commit

---
+++ interleaving_strings_with_minimum_cost.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minCost(self, s1, s2):
+        n, m = len(s1), len(s2)
+        dp = [[0]*(m+1) for _ in range(n+1)]
+        
+        for i in range(n):
+            for j in range(m):
+                if s1[i] == s2[j]:
+                    dp[i+1][j+1] = dp[i][j]
+                else:
+                    dp[i+1][j+1] = min(dp[i][j+1], dp[i+1][j]) + 1
+                    
+        return dp[n][m]
